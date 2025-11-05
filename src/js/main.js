0830c3476af6ef905b8655b4d3cd93f436173ac2
claradelvecchio <clara.anquetil@studentfr.ch>
2025-11-05 11:51:13 +0100

    frist-scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,24 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.to(".zoom-container", {
-  scrollTrigger: ".zoom-container", // start the animation when ".box" enters the viewport (once)
-  x: 500,
+gsap.registerPlugin(ScrollTrigger);
+
+// === Timeline liée au scroll ===
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".zoom-container",
+    start: "top top",
+    end: "bottom+=6000 top",
+    scrub: true,
+    pin: true,
+    markers: false, // mets true pour voir les repères
+  },
 });
+
+// === Séquence d’animations ===
+tl.to(".zoom-01", { scale: 3, y: 2800, duration: 30, ease: "none" })
+  .to(".zoom-02", { scale: 3, y: 2800, duration: 30, ease: "none" })
+  .to(".zoom-03", { scale: 3, y: 2800, duration: 30, ease: "none" })
+  .to(".zoom-04", { scale: 3, y: 2800, duration: 30, ease: "none" })
+  .to(".zoom-05", { scale: 3, y: 2800, duration: 30, ease: "none" })
+  .to(".zoom-06", { y: -1000, duration: 30, ease: "none" });
