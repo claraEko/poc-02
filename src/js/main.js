5218a27f781751177d22fc372ec6adf414c19371
claradelvecchio <clara.anquetil@studentfr.ch>
2025-11-06 10:14:49 +0100

    pause :33

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,7 @@ const tl = gsap.timeline({
   scrollTrigger: {
     trigger: ".zoom-container",
     start: "top top",
-    end: "bottom+=6000 top",
+    end: "bottom+=2500% top",
     scrub: true,
     pin: true,
     markers: false, // mets true pour voir les repères
@@ -16,9 +16,10 @@ const tl = gsap.timeline({
 });
 
 // === Séquence d’animations ===
-tl.to(".zoom-01", { scale: 3, y: 2800, duration: 30, ease: "none" })
-  .to(".zoom-02", { scale: 3, y: 2800, duration: 30, ease: "none" })
-  .to(".zoom-03", { scale: 3, y: 2800, duration: 30, ease: "none" })
-  .to(".zoom-04", { scale: 3, y: 2800, duration: 30, ease: "none" })
-  .to(".zoom-05", { scale: 3, y: 2800, duration: 30, ease: "none" })
-  .to(".zoom-06", { y: -2000, duration: 30, ease: "none" });
+tl.to(".zoom-01", { scale: 3, y: 2800, duration: 6, ease: "none" })
+  .to(".zoom-02", { scale: 3, y: 2800, duration: 6, ease: "none" })
+  .to(".zoom-03", { scale: 3, y: 2800, duration: 6, ease: "none" })
+  .to(".zoom-04", { scale: 3, y: 2800, duration: 6, ease: "none" })
+  .to(".zoom-05", { scale: 3, y: 2800, duration: 6, ease: "power1.out" })
+  .to(".zoom-06", { y: -2000, duration: 4, ease: "power1.in" }, "-=3")
+  .to(".scroll-h", { x: -3820, duration: 4, ease: "power1.in" });
