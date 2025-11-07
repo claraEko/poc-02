62b35bb84fd4e0b93b6c9210260a35407069f3a2
claradelvecchio <clara.anquetil@studentfr.ch>
2025-11-07 15:40:36 +0100

    fini sans anotation

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,7 @@ const tl = gsap.timeline({
   scrollTrigger: {
     trigger: ".zoom-container",
     start: "top top",
-    end: "bottom+=2500% top",
+    end: "bottom+=3500% top",
     scrub: true,
     pin: true,
     markers: false, // mets true pour voir les repères
@@ -16,10 +16,15 @@ const tl = gsap.timeline({
 });
 
 // === Séquence d’animations ===
-tl.to(".zoom-01", { scale: 3, y: 2800, duration: 6, ease: "none" })
-  .to(".zoom-02", { scale: 3, y: 2800, duration: 2, ease: "none" })
-  .to(".zoom-03", { scale: 3, y: 2800, duration: 6, ease: "none" })
-  .to(".zoom-04", { scale: 3, y: 2800, duration: 6, ease: "none" })
-  .to(".zoom-05", { scale: 3, y: 2800, duration: 6, ease: "power1.out" })
-  .to(".zoom-06", { y: "-66%", duration: 4, ease: "power1.in" }, "-=3")
-  .to(".scroll-h", { x: "-100%", duration: 4, ease: "power1.in" });
+tl.to(".zoom-1-5", { scale: 3, y: "100%", duration: 5, ease: "none" })
+  .to(".zoom-01", { scale: 3, opacity: 0, duration: 1, ease: "none" }, "-=3")
+  .to(".zoom-2-5", { scale: 3, y: "100%", duration: 8, ease: "none" }, "-=3")
+  .to(".zoom-02", { scale: 3, opacity: 0, duration: 1, ease: "none" }, "-=3")
+  .to(".zoom-3-5", { scale: 3, y: "100%", duration: 8, ease: "none" }, "-=3")
+  .to(".zoom-03", { scale: 3, opacity: 0, duration: 1, ease: "none" }, "-=3")
+  .to(".zoom-4-5", { scale: 3, y: "100%", duration: 8, ease: "none" }, "-=3")
+  .to(".zoom-04", { scale: 3, opacity: 0, duration: 1, ease: "none" }, "-=3")
+  .to(".zoom-5-5", { scale: 3, y: "100%", duration: 8, ease: "none" }, "-=3")
+  .to(".zoom-05", { scale: 3, opacity: 0, duration: 1, ease: "none" }, "-=3")
+  .to(".zoom-06", { y: "-66%", duration: 4, ease: "power1.in" }, "-=1")
+  .to(".scroll-h", { x: "-100%", duration: 10, ease: "power1.in" });
